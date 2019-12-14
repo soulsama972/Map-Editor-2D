dd33e88cd950e37be2d025be577ef9e30ce7b42b
soulsama <michael77700@gmail.com>
2019-12-14 13:15:38 +0200

    update

--- chupa/main.cpp
+++ chupa/main.cpp
@@ -7,28 +7,13 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
 {
-<<<<<<< HEAD
 	Window bShooter;
 	
 	bShooter.Init(L"bubbleShooter", 800, 600);
-	
-
 	while (bShooter.LoopEvent())
 	{
 		bShooter.ClearTargetView({ 0.2,0.2,0.2,1.0 });
 
-=======
-	
-
-	Window bShooter;
-	
-	bShooter.Init(L"bubbleShooter", 800, 600);
-	BubbleShooter* b = new BubbleShooter(bShooter.GetDeovce(),bShooter.GetScreen());
-	while (bShooter.LoopEvent())
-	{
-		bShooter.ClearTargetView({ 0.2,0.2,0.2,1.0 });
-		b->Draw();
->>>>>>> a1299912828782dc60d5cdf84eb7dfba028dd530
 		bShooter.Render();
 	}
 	return 0;
